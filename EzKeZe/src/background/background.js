911430bff46a3da65e47213480101ac51b067788
Matthew Roseman <mrr77@case.edu>
2016-04-16 18:47:41 -0400

    Added all the funcitons for the encrypt.js library
    Signed-off-by: Matthew Roseman <mrr77@case.edu>

--- EzKeZe/src/background/background.js
+++ EzKeZe/src/background/background.js
@@ -1,16 +1,18 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+var current_chat;
+var current_chat_key;
+var current_chat_iv;
 
-// Called when the user clicks on the browser action.
-console.log("Shit Happened");
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Hello World!');
-  chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action == "encrypt") {
+        var plaintext = request.text;
+        var ciphertext = encrypt_sym_message(plaintext, current_chat_key, current_chat_iv);        
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {action: "encrypted_text", text: ciphertext}, function(response) {
+                console.log(response);
+            });
+        });
+    }
+    else if (request.action == "sync") {
 
+    }
 });
-
-function doStuffWithDom(domContent) {
-    console.log('I received the following DOM content:\n' + domContent);
-}
