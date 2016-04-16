5bf8aa93a7cbbac5e834cfbff9f7f3f10b0f4627
pascalrascal <blaisemarchetti@outlook.com>
2016-04-16 18:59:46 -0400

    Added my things
    i hop eu like it

--- EzKeZe/src/content/content.js
+++ EzKeZe/src/content/content.js
@@ -1,15 +1,123 @@
 // Listen for messages
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(
+  function (msg, sender, sendResponse) {
+    console.log("message received");
     // If the received message has the expected format...
-    if (msg.text === 'report_back') {
+    if (msg.text == 'report_back') {
         // Call the specified callback, passing
         // the web-page's DOM content as argument
         sendResponse(document.all[0].outerHTML);
     }
+
+    if(msg.text == 'encrypt'){
+      console.log('ENCRYPT????');
+      var txt = textNodesUnder(document.activeElement);
+      sendReponse(txt[0]);
+    }
 });
 
 document.addEventListener('DOMNodeInserted', nodeInsertedCallback);
+
 function nodeInsertedCallback(event){
+  var node2search = event.target;
+  console.log("-----------------iframe changed---------------------");
   console.log(event);
+  //findText(event.target, "test");
+  highlight(node2search, "test");
+
 }
+
 console.log("shitstain paeg loaded");
+
+/**
+ * Takes html object and regex, and highlights the text that matches the regex
+ * @param html the html object that will be searched
+ * @param regex the regular expression to match to the html text
+ */
+
+
+ function textNodesUnder(el){
+  var n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
+  while(n=walk.nextNode()) a.push(n);
+  return a;
+}
+
+var findText = function(node, text){
+  var textNodes = textNodesUnder(node);
+  for(i in textNodes){
+    if(textNodes[i].textContent.search(text) != -1){
+      console.log(textNodes[i]);
+    }
+  }
+}
+
+
+function highlight(searchNode, regexString)  {
+
+    if (searchNode.nodeName == "MARK" ||
+        searchNode.nodeName == "SCRIPT" ||
+        searchNode.nodeName == "NOSCRIPT" ||
+        searchNode.nodeName == "STYLE" ||
+        searchNode.nodeType == Node.COMMENT_NODE) {
+
+            return;
+    }
+
+    // if this is a nonempty text node
+    if (searchNode.nodeType === Node.TEXT_NODE && (/[^\t\n\r ]/.test(searchNode.nodeValue))) {
+        var nodeValue, before, match, after; // string value of nodes
+        var regex; // the Regular Expression
+        var result; // result of regex.exec("string");
+        var parentNode;
+        var beforeNode, matchNode, afterNode; // the new nodes being added
+        var span; // the node being added above the match for highlighting
+        do {
+            nodeValue = searchNode.nodeValue;
+            regex = new RegExp(regexString, "g");
+            result = regex.exec(nodeValue);
+            // if a match was found
+            if (result != null) {
+                console.log("found match: " + result);
+
+                parentNode = searchNode.parentNode;
+
+                before = nodeValue.substring(0, result.index); // the string up to the first match
+                match = nodeValue.substring(result.index, regex.lastIndex); // the string of matched text
+                after = nodeValue.substring(regex.lastIndex); // the rest of the text (still to be matched)
+
+                console.log("The string: " + result[0] + " matches the search regular expression");
+
+                if (before) {
+                    beforeNode = document.createTextNode(before);
+                }
+
+                matchNode = document.createTextNode(match);
+
+
+                if (after) {
+                    afterNode = document.createTextNode(after);
+                    searchNode = afterNode;
+                // if this is the end of the text node
+                } else {
+                    searchNode = matchNode;
+                    break;
+                }
+            }
+        } while (result != null);
+    } else {
+        // So for some text boxes the hidden tag is set, so I'm going to continue to check everything hidden or not unless I figure out a better solution
+        // if this is a hidden node or not displayed in on screen
+        /*if ($(searchNode).css('display') == 'none' || $(searchNode).css('display') == 'hidden') {
+           return;
+        }*/
+    }
+
+    searchNode = searchNode.firstChild;
+    while (searchNode) {
+        highlight(searchNode, regexString);
+        searchNode = searchNode.nextSibling;
+    }
+
+    return;
+}
+highlight(document.body, "test");
