16fd42a76ec9d214c702fd3b43299836b607a45c
Matthew Roseman <mrr77@case.edu>
2016-04-16 15:11:47 -0400

    added messages form popup to content and background scripts
    Signed-off-by: Matthew Roseman <mrr77@case.edu>

--- EzKeZe/src/popup/popup.js
+++ EzKeZe/src/popup/popup.js
@@ -1,33 +1,21 @@
 $(document).ready(function() {
-    var chat_key; // the AES key for the current chat
-
     $("#encrypt").click(function() {
-        plaintext = getActiveText();
-        ciphertext = encrypt(plaintext, chat_key);
-        replaceActiveText(ciphertext);
+        // sends a message to te content scripts saying to encrypt the active text element
+        var request = JSON.parse("{\"action\": \"encrypt\"}");
+
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, request, function(response) {
+                console.log(response);
+            });
+        });
+    });
+
+    $("#sync").click(function() {
+        // sends a message to the background script saying to sync this users chat keys
+        var request = JSON.parse("{\"action\": \"sync\"}");
+
+        chrome.extension.sendMessage(request, function(response) {
+            console.log(response);
+        });
     });
 });
-
-/**
- * grabs the current text in active textfield
- * @return: a string representing the text
- */
-function getActiveText() {
-    return $(':focus').val();
-}
-
-/**
- * takes a plaintext and encrypts it based of the active chat
- * @param: plaintext the plaintext string to encrypt
- * @param: current_chat_key the AES key for the currently active chat (chosen by user from dropdown)
- * @return: the AES encrypted ciphertext
- */
-function encrypt(plaintext, current_chat_key) {
-}
-
-/**
- * finds the currently active text area dn replaces the current text
- * @param: replacement_text the text that will replace whatever is in the text area
- */
-function replaceActiveText(replacement_text) {
-}
