34d03ee151373510ae55c2235aa42c405376268f
gatesyp <ig11@zips.uakron.edu>
2016-06-30 14:46:00 -0400

    added small change

--- EzKeZe/src/dashboard/mic.js
+++ EzKeZe/src/dashboard/mic.js
@@ -204,6 +204,7 @@ function drawBuffer( width, height, context, data ) {
 
 var createKeyList = function(list){
   var lst = document.getElementById('keyList');
+  // clean the list before we append to it
   console.log(list);
   for(var i = 0; i < list.length; i++){
     var entry = list[i];
@@ -212,6 +213,11 @@ var createKeyList = function(list){
     var entryKey = entry.key;
     var rawHTML = '<span class="mdl-list__item-primary-content"> <i class="material-icons  mdl-list__item-avatar">lock</i>' +
         'Unlock Wtih Custom Key' + '</span> <span class="mdl-list__item-secondary-action"> <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="list-checkbox-1"> <input type="checkbox" id="list-checkbox-1" class="mdl-checkbox__input"/> </label> </span>';
+  //x = document.getElementById("keyList"), 
+    //c = x.childNodes;
+	//for (var i = 0; i < 4; i++) {
+		   //x.removeChild(c[i--]);
+	//}
     var listingItem = document.createElement('li');
     listingItem.className = "mdl-list__item";
     listingItem.innerHTML = rawHTML;
