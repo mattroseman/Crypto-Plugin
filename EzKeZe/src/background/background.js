9d6f50d33f69a41badbe650ea5b9c31d721c5702
Matthew Roseman <mrr77@case.edu>
2016-06-29 22:06:40 -0400

    nacl module is loading now
    Signed-off-by: Matthew Roseman <mrr77@case.edu>

--- EzKeZe/src/background/background.js
+++ EzKeZe/src/background/background.js
@@ -1,12 +1,27 @@
 var current_chat;
 var current_chat_key;
 
+var encrypt_module = null;
+
 //This is the key used to encode all the chats at the moment, it is temporary until the backend is up and running
 current_chat_key = [115, 98, 103, 111, 108, 121, 112, 105, 101, 115, 114, 100, 97, 105, 105, 97];
 
 console.log("fuck");
 
 window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+
+document.getElementById('listener').addEventListener('load', moduleDidLoad, true); 
+
+function moduleDidLoad() {
+    console.log("module loaded");
+    encrypt_module = document.getElementById('nacl_module');
+
+    encrypt_module.addEventListener('message', handleMessage, false);
+}
+
+function handleMessage(message) {
+    console.log("message from NaCl");
+}
 
 function onInitFs(fs) {
   fs.root.getFile('blaiseFUCKINGmarchetti.txt', {create: true}, function(fileEntry) {
@@ -60,6 +75,7 @@ window.webkitStorageInfo.requestQuota(PERSISTENT, 5*1024*1024, function(grantedB
 });
 
 
+
 //The listener for keyboard commands from the user
 chrome.commands.onCommand.addListener(function(command){
 
@@ -73,11 +89,12 @@ chrome.commands.onCommand.addListener(function(command){
 	      if(!response) console.log("response was null");
         //Encrypt the string that the active page sends you
         //var ciphertext = encrypt_sym_message(response.text, current_chat_key);
+        console.log('test');
         var generate_keys_context = {
             request_type: "generate_rsa_keys",
             password: "P@ssw0rd"
         };
-        background.naclModule.postMessage(generate_keys_context);
+        encrypt_module.postMessage(generate_keys_context);
 
         /*
         //Add the encrypted text to an object with an identifier
@@ -133,29 +150,3 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     console.log("The Extension Button was Clicked");
     chrome.tabs.create({'url': chrome.extension.getURL('src/dashboard/index.html'), 'selected': true});
 });
-
-function attachDefaultListeners() {
-    //  The NaCl module embed is created within the listenerDiv
-    var listenerDiv = document.getElementById('listener');
-
-    //  register the handle Message function as the message event handler.
-    listenerDiv.addEventListener('message', handleMessage, true);
-}
-
-function handleMessage(message) {
-    //  handle a message from NaCl
-    console.log(message.data);
-
-        //Add the encrypted text to an object with an identifier
-        var encryptedmessage =  {'identifier': 'MmmmMMMMmmmm_Dat_Some_GOOOOOOOOOOD_encRYPTION_mmmmMMMMMmmmmMM', 'content': ciphertext};
-        //Turn the object into a JSON formatted string
-        var messageString = JSON.stringify(encryptedmessage);
-        //Create an object to pass on to the active page
-        var data = {action: 'encrypted_text', text: messageString};
-
-        //Send the complete object to the active page
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, data, function(response) {
-            });
-        });
-}
